11c0bac1d358f4dbd23559b7b63530a380858015
Korakod <ice9393th@gmail.com>
2026-06-05 11:52:37 +0700

    update:home page dialog

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,20 @@ import {  useForm, } from "react-hook-form";
 
 export default function Home() {
   const state = useOverlayState()
-  const {register,handleSubmit} = useForm()
-  const {data,SetData} = useState("")
+  const {register,handleSubmit} = useForm({
+    defaultValues:{
+      username:"user",
+    }
+  })
+  const {data,setData} = useState()
+  const option = ["listener","speaker"] 
+  const [selectedOption, setSelectedOption] = useState("")
+  const [mode, setMode] = useState("")
 
+// mode = "speaker"
+// mode = "listener"
   function onSubmit(data) {
-    alert(data.total_people)
+    console.log(JSON.stringify(data))
     state.close()
   }
 
@@ -28,31 +37,51 @@ export default function Home() {
             <h1 className="text-2xl">สร้างห้องและพูดคุยกับผู้ฟัง 2 คน ที่พร้อมรับฟังโดยไม่ตัดสินคุณ</h1>
           </div>
           <div className="flex justify-evenly mt-10 ">
-            <Button className="font-bold bg-[#44444E] text-xl shadow-sm shadow-[#D3DAD9]" onPress={state.open} >เริ่มระบาย</Button>
-            <Button className="font-bold bg-[#44444E] text-xl  shadow-sm shadow-[#D3DAD9]">เข้าร่วมเป็นผู้ฟัง</Button>
+            {option.map((option)=>{
+              return(
+                <Button key={option} className="font-bold bg-[#44444E] text-xl shadow-sm shadow-[#D3DAD9]" 
+                onPress={() => {
+      setSelectedOption(option)
+      state.open()
+    }}
+                  >
+                    {option}
+            </Button>
+              )
+            })}
           </div>
           {/*<----- DialogUI ------>*/}
-          <Modal.Backdrop isOpen={state.isOpen} onOpenChange={state.setOpen} >
+          <Modal.Backdrop isOpen={state.isOpen} onOpenChange={state.setOpen} >  
         <Modal.Container >
           <Modal.Dialog className="sm:max-w-[360px] bg-[#44444E] text-[#D3DAD9]">
             <Modal.Header>
               <Modal.Icon>
                <UserRound />
               </Modal.Icon>
-              <Modal.Heading>สร้างห้องและพูดคุยกับผู้ฟัง</Modal.Heading>
+              <Modal.Heading>{selectedOption === "listener" ? "สร้างห้องและพูดคุยกับผู้ฟัง" : "สุ่มเลือกผู้ฟังโดยการเลือกประเภท"}</Modal.Heading>
             </Modal.Header>
             <Modal.Body>
-            <form onSubmit={handleSubmit(onSubmit)} className="grid">
+            {selectedOption === "listener" ? (<form onSubmit={handleSubmit(onSubmit)} className="grid">
               <select {...register("total_people", { required: true })} className="border-2 border-[#37353E] p-2 rounded-2xl bg-[#37353E]">
         <option value="" className="">จำนวนผู้รับฟัง...</option>
         <option value="1">1</option>
         <option value="2">2</option>
       </select>
-      <input type="submit" className="border-2 rounded-2xl mt-2 p-2 border-[#37353E] text-[#D3DAD9] bg-[#37353E]"/>
-            </form>
+      <input type="submit" className="border-2 rounded-2xl mt-2 p-2 border-[#37353E] text-[#D3DAD9] bg-[#37353E] hover:bg-[#37353E]/80 "/>
+            </form>) : selectedOption === "speaker" ? (<form onSubmit={handleSubmit(onSubmit)} className="grid">
+              <select {...register("choose_client", { required: true })} className="border-2 border-[#37353E] p-2 rounded-2xl bg-[#37353E]">
+        <option value="" className="">เลือกประเภทผู้ฟัง</option>
+        <option value="love">เรื่องความรัก</option>
+        <option value="job">เรื่องงาน</option>
+        <option value="family">เรื่องครอบครัว</option>
+        <option value="other">เรื่องอื่นๆ..</option>
+      </select>
+      <input type="submit" className="border-2 rounded-2xl mt-2 p-2 border-[#37353E] text-[#D3DAD9] bg-[#37353E] hover:bg-[#37353E]/80 "/>
+            </form>) : ""}
+          
             </Modal.Body>
             <Modal.Footer>
-              <Button className="w-full -mt-2" slot="close" variant="danger" >
+              <Button className="w-full -mt-2 p-2" slot="close" variant="danger" >
                 ยกเลิก
               </Button>
             </Modal.Footer>
