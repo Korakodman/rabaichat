c124b27afaf8af7d8b6882f4a52eb67e8c9c8644
Korakod <ice9393th@gmail.com>
2026-06-28 12:46:20 +0700

    add: mock message

--- src/app/Chat-Room/page.js
+++ src/app/Chat-Room/page.js
@@ -1,8 +1,45 @@
 import React from 'react'
 
 function page() {
+
+
+  const message = [{
+    textMessage:"chat-1",
+    name:"korakod",
+    role:"speaker",
+    id:1
+  },{
+    textMessage:"chat-2",
+    name:"jame",
+     role:"listen",
+    id:2
+  },
+  {textMessage:"chat-3",
+    name:"bond",
+    role:"listen",
+    id:3
+  }]
+
   return (
-    <div>Chat-Room</div>
+    <main className='p-2 h-screen  '>
+      {/* header */}
+     <header>Chat-Room</header>
+     {/* bg-chat-history */}
+      <section className=' relative flex-col  h-11/12 border-2 border-black'>
+       <div className='grid'>
+         <div className='flex-1 overflow-y-auto p-4 space-y-2'>
+          {message.map((msg)=>
+          {return(
+            <div key={msg.id} className={`flex ${msg.id === 2 ? "justify-end" : " justify-start"}`}>
+              {msg.textMessage}
+              <span>{msg.name}</span>
+              </div>
+          )})}
+         </div>
+       </div>
+      </section>
+      <section></section>
+    </main>
   )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased `}
     >
-      <body className="min-h-screen"><div className="flex min-h-screen">
+      <body className="min-h-screen"><div className="flex h-screen">
         <MainNavbar/> 
         <main className="min-w-0 flex-1 overflow-x-hidden bg-[#37353E]">{children}</main>
       </div></body>
